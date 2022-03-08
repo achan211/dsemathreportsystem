f0ce2afd57dee9251b3ade76f5f7853b63b34c88
Alvin Chan <alvinmychan@gmail.com>
2022-03-08 16:01:52 +0800

    Update 2019 questions

--- script.js
+++ script.js
@@ -9,13 +9,9 @@ var data = [
     },
     {
         "year": "2019", 
-        "questions": ["Change of Subject", "Simplification"]
+        "questions": ["Change of Subject", "Simplification", "Quadratic equations + Properties of Quadrilaterals", "Factorization", "Percentages", "Inequality", "Ratio + Simultaneous Linear Equations", "Pie Chart + Probability", "Volume & Surface Areas", "Variations", "Factor Theorem", "Stem-and-Leaf + Measures of Dispersions", "Properties of Circles", "Congruent & Similar Triangles + Properties of Quadrilaterals", "Permutations and Combinations", "Sum & Product of Roots + Sequences + Logarithms", "Coordinate Geometry + Locus", "3D Trigonometry", "Completing the Square + Transformations of Graphs + Properties of Circles"]
     }
 ]
-
-function Test(){
-    alert("Test is successful!");
-}
 
 function byYear(Year){
     var div = document.querySelector('#yearResult');
@@ -34,6 +30,7 @@ function generateYearReport(i){
     for (var x = 0; x < questions.length; x++){
         queNo = x + 1;
         let newQuestion = document.createElement("p");
+        newQuestion.className = "lead";
         newQuestion.innerHTML = "Question " + queNo + " is " + questions[x];
         document.querySelector("#yearResult").appendChild(newQuestion);
     }
